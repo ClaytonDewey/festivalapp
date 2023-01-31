e5c601f1e99f5aab3dbabfa8e5d987a6a348b8ad
ClaytonDewey <clay@dryadmedia.com>
2023-01-31 14:52:35 -0600

    import reportWebVitals

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
 import 'antd/dist/antd.css';
+import reportWebVitals from './reportWebVitals';
+
 
 import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
